f8178f57a00112f0ddbd8df169e54bff108d908f
Stan004 <flywwww004@gmail.com>
2024-06-29 18:27:57 +0800

    organize timeline compoment code: use map

--- src/components/Timline.jsx
+++ src/components/Timline.jsx
@@ -36,67 +36,26 @@ const timelineData = [{
 
 export default function Timeline(props){
     return(
+        
         <div className="timeline">
-            <div className="timeline__item">
-                <div className="timeline__left">
-                    <h3 className="timeline__company">{timelineData[0].companyName}</h3>
-                    <small><p className="timeline__duration">{timelineData[0].duration}</p></small>
+            {timelineData.map( (e, index) => (
+                <div className="timeline__item" key={index}>
+                    <div className="timeline__left">
+                        <h3 className="timeline__company">{e.companyName}</h3>
+                        <small><p className="timeline__duration">{e.duration}</p></small>
+                    </div>
+                    <div className="timeline__right">
+                        <h4 className="timeline__title">{e.title}</h4>
+                        <ul className="timeline__highlights">
+                            <li>{e.highlight1}</li>
+                            <li>{e.highlight2}</li>
+                            <li>{e.highlight3}</li>
+                            <li>{e.highlight4}</li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="timeline__right">
-                    <h4 className="timeline__title">{timelineData[0].title}</h4>
-                    <ul className="timeline__highlights">
-                        <li>{timelineData[0].highlight1}</li>
-                        <li>{timelineData[0].highlight2}</li>
-                        <li>{timelineData[0].highlight3}</li>
-                        <li>{timelineData[0].highlight4}</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="timeline__item">
-                <div className="timeline__left">
-                    <h3 className="timeline__company">{timelineData[1].companyName}</h3>
-                    <small><p className="timeline__duration">{timelineData[1].duration}</p></small>
-                </div>
-                <div className="timeline__right">
-                    <h4 className="timeline__title">{timelineData[1].title}</h4>
-                    <ul className="timeline__highlights">
-                        <li>{timelineData[1].highlight1}</li>
-                        <li>{timelineData[1].highlight2}</li>
-                        <li>{timelineData[1].highlight3}</li>
-                        <li>{timelineData[1].highlight4}</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="timeline__item">
-                <div className="timeline__left">
-                    <h3 className="timeline__company">{timelineData[2].companyName}</h3>
-                    <small><p className="timeline__duration">{timelineData[2].duration}</p></small>
-                </div>
-                <div className="timeline__right">
-                    <h4 className="timeline__title">{timelineData[2].title}</h4>
-                    <ul className="timeline__highlights">
-                        <li>{timelineData[2].highlight1}</li>
-                        <li>{timelineData[2].highlight2}</li>
-                        <li>{timelineData[2].highlight3}</li>
-                        <li>{timelineData[2].highlight4}</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="timeline__item">
-                <div className="timeline__left">
-                    <h3 className="timeline__company">{timelineData[3].companyName}</h3>
-                    <small><p className="timeline__duration">{timelineData[3].duration}</p></small>
-                </div>
-                <div className="timeline__right">
-                    <h4 className="timeline__title">{timelineData[3].title}</h4>
-                    <ul className="timeline__highlights">
-                        <li>{timelineData[3].highlight1}</li>
-                        <li>{timelineData[3].highlight2}</li>
-                        <li>{timelineData[3].highlight3}</li>
-                        <li>{[3].highlight4}</li>
-                    </ul>
-                </div>
-            </div>
+            ))}
         </div>
     )   
 }
+
